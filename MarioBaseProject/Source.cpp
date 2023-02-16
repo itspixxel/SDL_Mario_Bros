609a180ab5d4ba0360e7a22ee6d806e7657aa1b0
Sarthak (Pixx) Sachdeva <115170653+itspixxel@users.noreply.github.com>
2023-02-16 16:11:07 +0000

    initial commit

---
+++ MarioBaseProject/Source.cpp
@@ -0,0 +1,69 @@
+//Be grateful for humble beginnings, because the next level will always require so much more of you
+// that's deep^^
+
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include "constants.h"
+
+using namespace std;
+
+//Function prototypes
+bool InitSDL();
+void CLoseSDL();
+
+//Globals
+SDL_Window* g_window = nullptr;
+
+int main(int argc, char* args[])
+{
+	//check if sdl was setup correctly
+	if (InitSDL())
+	{
+		SDL_Delay(5000);
+	}
+
+	CLoseSDL();
+
+	return 0;
+}
+
+bool InitSDL()
+{
+	//Setup SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL did not initialise. Error: " << SDL_GetError();
+		return false;
+	}
+	else
+	{
+		//setup passed so create window
+		g_window = SDL_CreateWindow("Games Engine Creation",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN);
+		//did the window get created?
+		if (g_window == nullptr)
+		{
+			//window failed
+			cout << "Window was not created. Error: " << SDL_GetError();
+			return false;
+		}
+	}
+
+}
+
+void CLoseSDL()
+{
+	//release the window
+	SDL_DestroyWindow(g_window);
+	g_window = nullptr;
+
+	//quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
+}
---
+++ MarioBaseProject/constants.h
@@ -0,0 +1,4 @@
+#pragma once
+//screen Dimensions
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 768
